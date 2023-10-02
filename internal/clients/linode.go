a2de4a953844ce44f626100209662072efe10b11
Dasha Komsa <komsa.darya@gmail.com>
2023-10-02 15:04:12 -0400

    configure shared terraform providers

--- internal/clients/linode.go
+++ internal/clients/linode.go
@@ -35,7 +35,7 @@ const (
 
 // TerraformSetupBuilder builds Terraform a terraform.SetupFn function which
 // returns Terraform provider setup configuration
-func TerraformSetupBuilder(version, providerSource, providerVersion string) terraform.SetupFn {
+func TerraformSetupBuilder(version, providerSource, providerVersion string, scheduler terraform.ProviderScheduler) terraform.SetupFn {
 	return func(ctx context.Context, client client.Client, mg resource.Managed) (terraform.Setup, error) {
 		ps := terraform.Setup{
 			Version: version,
@@ -43,6 +43,7 @@ func TerraformSetupBuilder(version, providerSource, providerVersion string) terr
 				Source:  providerSource,
 				Version: providerVersion,
 			},
+			Scheduler: scheduler,
 		}
 
 		configRef := mg.GetProviderConfigReference()
