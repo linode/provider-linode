3f953dbbd9568fdd80a30487bc94bd1ebbb47139
Tarun Chinmai Sekar <schinmai@akamai.com>
2026-08-24 12:53:00 -0700

    chore: make generate

--- apis/lke/v1alpha1/zz_generated.resolvers.go
+++ apis/lke/v1alpha1/zz_generated.resolvers.go
@@ -7,7 +7,6 @@ package v1alpha1
 
 import (
 	"context"
-
 	reference "github.com/crossplane/crossplane-runtime/pkg/reference"
 	errors "github.com/pkg/errors"
 	client "sigs.k8s.io/controller-runtime/pkg/client"
